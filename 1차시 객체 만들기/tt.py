031eddc8b10b86a28e20078c05c40eb99b8f6c60
psh <sfgussf@naver.com>
2025-09-10 14:03:09 +0900

    aasd

--- 1차시 객체 만들기/tt.py
+++ 1차시 객체 만들기/tt.py
@@ -1,12 +1,33 @@
 from ursina import *
 
+
 app = Ursina()
 
-# 화면에 글자 출력
-message = Text(text='Hello Ursina!', scale=2, color=color.yellow, position=(0,0))
-
-# 3D 공간에도 배치 가능 (parent를 scene으로 설정)
-label = Text(text='이건 3D 공간', world_z=2, parent=scene, color=color.red)
-
 EditorCamera()
+
+class object(Entity):
+    def __init__(self, model, x, z):
+        super().__init__(
+            model = model,
+            color = "#8d6d8d",
+            scale = (1,1,1),
+            position = (x,0,z),
+            collider = 'box',
+            parent=pivot
+        )
+    
+    def update(self):
+        self.rotation_y += 10 * time.dt
+        if self.intersects():
+            print('충돌함')
+
+pivot = Entity()
+a = object('sphere',0,0)
+b = object('cube',5,5)
+c = object('cube', 10,10)
+
+def update():
+    pivot.rotation_y += 20 * time.dt
+
+
 app.run()
